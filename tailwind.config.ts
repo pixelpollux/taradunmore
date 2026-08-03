ef4471043a5ef14d2fff6621bc29241d697fa5f4
pixelpollux <terasmash@gmail.com>
2026-08-03 15:54:53 -0500

    css and tailwind config fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,21 +2,22 @@ import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
 export default {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./lib/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-Fredoka)"],
-        cherry: ["var(--font-cherry)"],
-      },
-    },
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [typography],
+	content: [
+		"./app/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./lib/**/*.{ts,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Fredoka", "sans-serif"],
+				bagel: ["Bagel Fat One", "serif"],
+				cherry: ["Cherry Bomb One", "serif"],
+			},
+		},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [typography],
 } satisfies Config;
